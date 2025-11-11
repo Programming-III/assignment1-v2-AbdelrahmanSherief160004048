8bcdc315fcc50af4f131f69331c8c4c275e62e71
AbdelrahmanSherief160004048 <dottysherief@gmail.com>
2025-11-11 09:58:55 +0200

    Update Person.h

--- Person.h
+++ Person.h
@@ -6,7 +6,39 @@ using namespace std;
 
 //#write Person class here
 
+class person
+{
+private :
+       string name;
+       int id;
+public :
+    person(string n , int i);
+    display();
+    string getName();
+    int getId();
+};
+person ::person(string n , int i)
+{
 
+    this.name->n;
+    this.id=i;
+}
+string person ::getName()
+{
+    return name;
+}
+int person :: getId()
+{
+
+    return id;
+}
+
+void person :: display(name , id)
+{
+
+    cout<< "persons name" << name << endl;
+    cout<< "persons id" << id << endl;
+}
 
 
 
