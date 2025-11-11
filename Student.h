cefd6e60f0c9dadebbb372ae9ce89ce0d4cb989e
AbdelrahmanSherief160004048 <dottysherief@gmail.com>
2025-11-11 09:59:48 +0200

    Update Student.h

--- Student.h
+++ Student.h
@@ -9,6 +9,40 @@
 
 
 
+class student
+{
+private :
+    int yearLevel;
+    string major;
+public :
+    void student(int y , string m)
+    int getYearLevel();
+    string getMajor();
+    display();
+
+};
+void student :: student(int y , string m)
+{
+
+    this->yearLevel = y;
+    this->major=m
+}
+int student :: getYearLevel()
+{
+
+    return yearLevl;
+}
+string student :: getMajor()
+{
+
+    return major;
+}
+void student :: display()
+{
+
+    cout << "yearlevel is : " << yearLevel << endl;
+    cout << "major is " << major << endl;
+}
 
 
 
