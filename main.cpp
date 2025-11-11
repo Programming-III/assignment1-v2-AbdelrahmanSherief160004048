b015b4c5d93df308a3f2b342abce0e18c6559f4e
AbdelrahmanSherief160004048 <dottysherief@gmail.com>
2025-11-11 10:05:02 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,48 +1,150 @@
 #include <iostream>
-#include <string>
-#include "Person.h"
-#include "Student.h"
-#include "Instructor.h"
-#include "Course.h"
-
-
 using namespace std;
+#include<string>
+#ifndef person_H
+#define person_H
+#include "person.H"
+#include "student.H"
+#include "intructor.H"
+#include "course.H"
+class person
+{
+private :
+       string name;
+       int id;
+public :
+    person(string n , int i);
+    display();
+    string getName();
+    int getId();
+};
+person ::person(string n , int i)
+{
 
-// ==================== Person Class Implementation =========================
+    this.name->n;
+    this.id=i;
+}
+string person ::getName()
+{
+    return name;
+}
+int person :: getId()
+{
+
+    return id;
+}
+
+void person :: display(name , id)
+{
+
+    cout<< "persons name" << name << endl;
+    cout<< "persons id" << id << endl;
+}
+
+
+class student
+{
+private :
+    int yearLevel;
+    string major;
+public :
+    void student(int y , string m)
+    int getYearLevel();
+    string getMajor();
+    display();
+
+};
+void student :: student(int y , string m)
+{
+
+    this->yearLevel = y;
+    this->major=m
+}
+int student :: getYearLevel()
+{
+
+    return yearLevl;
+}
+string student :: getMajor()
+{
+
+    return major;
+}
+void student :: display()
+{
+
+    cout << "yearlevel is : " << yearLevel << endl;
+    cout << "major is " << major << endl;
+}
 
 
 
+class instructor{
+private:
+    string department;
+    int experienceYears;
+public :
+    void instuctor(string d , int e)
+    string getDepartment();
+    int getExperienceYears();
+    display();
+};
+void instructor :: intsructor(string d , int e)
+{
+
+    this->department = d;
+    this experienceYears=e;
+}
+string getDepartment()
+{
+
+    return department;
+
+}
+int getExperienceYears()
+{
+
+    return experienceYears;
+}
+void instructor :: display()
+{
+
+    cout << "department is :" << department << endl;
+    cout << "experienceYears is :" << experienceYears << endl;
+}
 
 
 
+class course
+{
+
+private :
+    string courseCode;
+    string courseName;
+    int maxStudents;
+    Student* students;
+    int currentStudents;
+public :
+    addStudents(const Student& s);
+    displayCourseInfo();
+
+};
+void course :: addStudents(const Student& s)
+{
+    Students = Students + s;
+
+}
+void course :: displayCourseInfo()
+{
+    cout << "course code is :" << courseCode << endl;
+    cout << "coursename is: " << courseName << endl;
+    cout << "maxstudents is :" << maxStudents << endl;
+    cout << "current students :" << currentStudents << endl;
+}
 
 
-// ==================== Student Class Implementation ====================
-
-
-
-
-
-
-// ==================== Instructor Class Implementation ====================
-
-
-
-
-
-
-// ==================== Course Class Implementation ====================
-
-
-
-
-
-
-
-
-// ==================== Main Function ====================
-int main() {
-   
-    
+int main()
+{
+    cout << "Hello world!" << endl;
     return 0;
 }
