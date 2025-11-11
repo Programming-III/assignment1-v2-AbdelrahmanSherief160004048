13e2a9bfe3a0000158b80801be1656a4ea2ddd68
AbdelrahmanSherief160004048 <dottysherief@gmail.com>
2025-11-11 10:00:29 +0200

    Update Instructor.h

--- Instructor.h
+++ Instructor.h
@@ -7,6 +7,39 @@ using namespace std;
 
 
 
+class instructor{
+private:
+    string department;
+    int experienceYears;
+public :
+    void instuctor(string d , int e)
+    string getDepartment();
+    int getExperienceYears();
+    display();
+};
+void instructor :: intsructor(string d , int e)
+{
+
+    this->department = d;
+    this experienceYears=e;
+}
+string getDepartment()
+{
+
+    return department;
+
+}
+int getExperienceYears()
+{
+
+    return experienceYears;
+}
+void instructor :: display()
+{
+
+    cout << "department is :" << department << endl;
+    cout << "experienceYears is :" << experienceYears << endl;
+}
 
 
 
